407a2c36c60954b27f706bd83e52b512273bf459
Marcos Antonio Prado <marcos.prado@CONTABILIZEI.COM.BR>
2021-07-10 19:24:28 -0300

    Adicionando dependencias utilizadas no projeto e configurando dockercompose e dockerfile

--- src/main/java/com/example/workshop/springboot/repository/UserRepository.java
+++ src/main/java/com/example/workshop/springboot/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-// Norberto Enomoto
+// Marcos Prado
 public interface UserRepository extends CrudRepository<UserModel, Integer>  {  
     Iterable<UserModel> findByName(String name);
 }
